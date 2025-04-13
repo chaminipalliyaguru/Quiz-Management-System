6a5581cc14eabf4580e409248ea6cf8674c97b6b
chaminipalliyaguru <bimalkachamini@gmail.com>
2025-04-13 17:24:50 +0530

    created start component

--- Frontend/src/Pages/Start_window.jsx
+++ Frontend/src/Pages/Start_window.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Start() {
   return (
-    <div>
-      <p>Ready to Start?</p>
-      <button>Start</button>
+    <div className="text-center pt-16 ">
+      <p className="text-3xl font-bold pb-10">Ready to Start?</p>
+      <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-xl shadow-md hover:bg-blue-700 transition duration-300">
+        Start Quiz
+      </button>
     </div>
-  )
+  );
 }
 
 export default Start;
