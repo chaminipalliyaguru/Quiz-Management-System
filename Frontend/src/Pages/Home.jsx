a3de5ba6800e99a016b84a0f37fba80575414505
chaminipalliyaguru <bimalkachamini@gmail.com>
2025-04-15 22:18:44 +0530

    frontend connect with backend

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
-import Start from "./Start_window"
+import { useEffect, useState } from "react";
+import Start from "./Start_window";
 
 function Home() {
   const [value, setValue] = useState("");
+  const [subject, setSubject] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/subjects")
+      .then((res) => res.json())
+      .then((data) => setSubject(data))
+      .catch((error) => console.error(error));
+  }, []);
 
   const handleChange = (subject) => {
     setValue(subject);
-
   };
 
   return (
     <>
-    <header className="text-center py-12">
+      <header className="text-center py-12">
         <h1 className="text-5xl font-bold text-gray-800 drop-shadow-md">
           Welcome to Knowledge Measurment Website
         </h1>
@@ -19,35 +26,20 @@ function Home() {
           Select a subject to begin your quiz
         </p>
       </header>
-    {!value ? (
-      
-      <div className="grid grid-cols-4 gap-4 pl-12 pr-14">
-        <button
-          className="bg-blue-500 h-28 rounded-3xl text-center text-5xl text-amber-50"
-          onClick={() => handleChange("Science")}
-        >
-          Science
-        </button>
-        <button
-          className="bg-pink-500 rounded-3xl text-center text-5xl text-amber-50"
-          onClick={() => handleChange("Mathematics")}
-        >
-          Mathematics
-        </button>
-        <button
-          className="bg-amber-500 rounded-3xl text-center text-5xl text-amber-50"
-          onClick={() => handleChange("Tamil")}
-        >
-          Tamil
-        </button>
-        <button
-          className="bg-emerald-500 rounded-3xl text-center text-5xl text-amber-50"
-          onClick={() => handleChange("English")}
-        >
-          English
-        </button>
-      </div>) : (
-        <Start subject={value}/>
+      {!value ? (
+        <div className="grid grid-cols-4 gap-4 pl-12 pr-14">
+          {subject.map((subject) => (
+            <button
+              key={subject.id}
+              className="bg-blue-500 h-28 rounded-3xl text-center text-5xl text-amber-50"
+              onClick={() => handleChange({subject})}
+            >
+              {subject.subject_name}
+            </button>
+          ))}
+        </div>
+      ) : (
+        <Start subject={value} />
       )}
     </>
   );
--- Frontend/src/Pages/Questions.jsx
+++ Frontend/src/Pages/Questions.jsx
@@ -1,11 +1,9 @@
-import {useState} from 'react'
-
-function Questions(){
+function Questions() {
   const question = {
-    text :'1. What is the capital of France?',
-    options : ['Berlin', 'Madrid', 'Paris', 'Lisbon'],
-    correctAnswer: 'paris',
-  }
+    text: "1. What is the capital of France?",
+    options: ["Berlin", "Madrid", "Paris", "Lisbon"],
+    correctAnswer: "paris",
+  };
 
   return (
     <div className="p-4 max-w-md mx-auto">
@@ -13,26 +11,19 @@ function Questions(){
       <p className="mb-4">{question.text}</p>
       <div>
         {question.options.map((options, index) => (
-          <div key={index} className='mb-2'>
+          <div key={index} className="mb-2">
             <label className="flex items-center">
-              <input
-                type="radio"
-                name="mcq"
-                value=''/>
-                {question.options[index]}
-              </label>
-              </div>
+              <input type="radio" name="mcq" value="" />
+              {question.options[index]}
+            </label>
+          </div>
         ))}
       </div>
-      <button
-       
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-      >
+      <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
         Submit
       </button>
     </div>
-  )
+  );
 }
-
 
 export default Questions;
