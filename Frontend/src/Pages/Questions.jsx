d24b2c42556d9fd5651bb466457a8d14f94e1aab
chaminipalliyaguru <bimalkachamini@gmail.com>
2025-04-15 13:18:18 +0530

    updated

--- Frontend/src/Pages/Questions.jsx
+++ Frontend/src/Pages/Questions.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import {useState} from 'react'
 
-function Questions() {
+function Questions(){
+  const question = {
+    text :'1. What is the capital of France?',
+    options : ['Berlin', 'Madrid', 'Paris', 'Lisbon'],
+    correctAnswer: 'paris',
+  }
+
   return (
-    <div>
-      <h1>hi</h1>
+    <div className="p-4 max-w-md mx-auto">
+      <h1 className="text-xl font-bold mb-4">Select most suitable answer.</h1>
+      <p className="mb-4">{question.text}</p>
+      <div>
+        {question.options.map((options, index) => (
+          <div key={index} className='mb-2'>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                name="mcq"
+                value=''/>
+                {question.options[index]}
+              </label>
+              </div>
+        ))}
+      </div>
+      <button
+       
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      >
+        Submit
+      </button>
     </div>
   )
 }
+
 
 export default Questions;
--- Frontend/src/Pages/Start_window.jsx
+++ Frontend/src/Pages/Start_window.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState} from "react";
+import { Link } from "react-router-dom";
 import Questions from "./Questions";
 
 function Start(subject) {
@@ -7,13 +8,13 @@ function Start(subject) {
   return (
     <div className="text-center pt-16 ">
       <p className="text-3xl font-bold pb-10">Ready to Start?</p>
-      {!startQuiz ? (
+   
+      <Link to = '/Questions'>
       <button className="px-6 py-3 bg-blue-600 text-white font-semibold rounded-xl shadow-md hover:bg-blue-700 transition duration-300" onClick={() => setStartQuiz(true)}>
         Start Quiz
       </button>
-      ) : (
-        <Questions value={subject}/>
-      )}
+      </Link>
+      
     </div>
   );
 }
